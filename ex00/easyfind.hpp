8301a65acd04ab472b0cfba0457af8569d9cb1af
Mayeul LANEYRIE <mlaneyri@student.42.fr>
2022-07-06 13:19:34 +0200

    ex01

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaneyri <mlaneyri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 18:29:55 by mlaneyri          #+#    #+#             */
+/*   Updated: 2022/07/06 13:17:02 by mlaneyri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <algorithm>
+
+template<typename T>
+typename T::iterator	easyfind(T& container, int const n)
+{
+	return (std::find(container.begin(), container.end(), n));
+}
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaneyri <mlaneyri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 16:16:34 by mlaneyri          #+#    #+#             */
+/*   Updated: 2022/07/06 13:19:01 by mlaneyri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "./easyfind.hpp"
+
+int	main( void )
+{
+	std::vector<int>	vect(42);
+	std::vector<int>::iterator	it;
+
+	for (int i = 0; i < 42; i++)
+		vect[i] = i;
+	it = easyfind(vect, 42);
+	if (it != vect.end())
+		std::cout << "Found!" << std::endl;
+	else
+		std::cout << "Not found." << std::endl;
+	return (0);
+}
